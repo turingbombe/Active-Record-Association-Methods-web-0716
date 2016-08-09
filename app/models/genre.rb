2e2dcd694b1f3afe81ade091d1cb6752b4fe2866
Charlie Cook <turingbombe@gmail.com>
2016-08-09 10:38:04 -0400

    member: Active Record Association

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,21 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.count 
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    # binding.pry
+    self.songs.count do |x|
+      x.artist_id
+    end
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    artists.collect do |artist|
+      artist.name
+    end
   end
 end
